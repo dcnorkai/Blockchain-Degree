a6a2918af9c85fec799c0a50d8cf102ea8bf1f64
dcnorkai <54694121+dcnorkai@users.noreply.github.com>
2022-10-26 15:37:45 -0400

    Update Degree.sol

--- Degree.sol
+++ Degree.sol
@@ -1,47 +1,82 @@
 // SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0; 
 
-pragma solidity ^0.6.0; 
-
-contract degree {
+contract Degree {
     address registrar;
+    struct Course{
+        string name;
+        uint gpa;
+    }
     struct Student{
         address studentPerson;
         uint hashOfDegreeDetails;
         uint overallGPA;
-        mapping (string=>uint) courses;
         bool graduationStatus;
     }
-    Student public defaultStudent;
-    mapping (address=>Student) public students;
+    struct Admin{
+        address adminPerson;
+    }
+    Student private defaultStudent;
+    Admin private defaultAdmin;
+    mapping(address => Student) private students;
+    mapping(address => Admin) private admins;
+    mapping(address => mapping(string => uint)) private courseIndex;
+    mapping(address => Course[]) private studentCourses;
 
-    // modifiers or rules
     modifier onlyRegistrar{
-        require(msg.sender==registrar);
+        require(msg.sender == registrar);
+        _;
+    }
+
+    modifier onlyStudent{
+        require(msg.sender == students[msg.sender].studentPerson);
+        _;
+    }
+
+    modifier onlyAdmin{
+        require(msg.sender == admins[msg.sender].adminPerson);
         _;
     }
 
     // constructor function
-    constructor() public {
+    constructor() {
         registrar = msg.sender;
     }
 
-    function setStudent(address studentPerson) onlyRegistrar public {
+    function setAdmin(address adminPerson) onlyRegistrar public {
+        Admin memory temp = defaultAdmin;
+        temp.adminPerson = adminPerson;
+        admins[adminPerson] = temp;
+    }
+
+    function setStudent(address studentPerson) onlyAdmin public {
         Student memory temp = defaultStudent;
         temp.studentPerson = studentPerson;
         students[studentPerson] = temp;
     }
 
-    function setGraduated(address studentPerson) onlyRegistrar public {
+    function setGraduated(address studentPerson) onlyAdmin public {
         students[studentPerson].graduationStatus = true;
     }
 
-    function setCourse(address studentPerson, string calldata courseName, uint courseGPA) onlyRegistrar public {
-        students[studentPerson].courses[courseName] = courseGPA;
+    function setCourse(address studentPerson, string memory courseName, uint courseGPA) onlyAdmin public {
+        Course[] storage temp = studentCourses[studentPerson];
+        uint newIndex = temp.length;
+        temp.push(Course({name : courseName, gpa : courseGPA}));
+        courseIndex[studentPerson][courseName] = newIndex;
     }
 
-    //For testing
-    function showCourse(address studentPerson, string calldata courseName) public view returns (uint) {
-        return students[studentPerson].courses[courseName];
+    function showCourses(address studentPerson) public view returns (Course[] memory) {
+        return studentCourses[studentPerson];
     }
 
+    function hideCourse(string memory courseName) onlyStudent public {
+        //remove the course passed in
+        //store the removed course in another data structure so it can be retrieved later
+    }
+
+    function showCourse(string memory courseName) onlyStudent public {
+        //add course back to courses data structure from the hidden courses data structure
+        //remove it from the hidden coursess data structure
+    }
 }
